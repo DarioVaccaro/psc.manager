50a7f878844039e1d3e824bb5ebfe7c1d4768e41
Dario Vaccaro <dvaccaro@hartenergy.com>
2019-04-18 16:16:40 -0500

    GUI Hello World

--- display.py
+++ display.py
@@ -1,5 +1,20 @@
+import tkinter
+
 class Display(object):
-    def __init__():
+    def __init__(self):
+        self.window = tkinter.Tk()
+
+    def label(self , text):
+        return tkinter.Label(self.window , text = text).pack()
+
+    def title(self , title):
+        self.window.title(title)
+        
+        return self
 
 if __name__ == '__main__':
-    print('Display')
+    display = Display()
+    display.title('Pro Shop Coordinator Manager')
+    display.label('Hello World!')
+
+    display.window.mainloop()
