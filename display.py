fa2b755b533570347c8339a8420ab0e0b2b4781b
Dario Vaccaro <dvaccaro@hartenergy.com>
2019-04-18 16:19:39 -0500

    GUI Hello World

--- display.py
+++ display.py
@@ -4,13 +4,13 @@ class Display(object):
     def __init__(self):
         self.window = tkinter.Tk()
 
-    def label(self , text):
-        return tkinter.Label(self.window , text = text).pack()
-
     def title(self , title):
         self.window.title(title)
         
         return self
+
+    def label(self , text):
+        return tkinter.Label(self.window , text = text).pack()
 
 if __name__ == '__main__':
     display = Display()
